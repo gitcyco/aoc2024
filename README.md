20f2da950f5b07ce1f76d44d0e2d5cb35b61aac9
gitcyco <gitcyco@users.noreply.github.com>
2024-12-02 21:52:56 -0800

    day 3

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 1.976ms
 
 ```
 Day 02
-Time part 1: 3.566ms
-Time part 2: 1395.2ms
-Both parts: 1398.766ms
+Time part 1: 4.376ms
+Time part 2: 4.109ms
+Both parts: 8.485ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 27.171ms
+Time part 2: 18.516ms
+Both parts: 45.686ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 1400.742ms
+Total stars: 6/50
+Total time: 56.148ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.js
@@ -0,0 +1,61 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  let ins = input.match(/mul\(\d{1,3},\d{1,3}\)/g);
+  let total = 0;
+  for (let mul of ins) {
+    let nums = mul
+      .split(",")
+      .map((e) => e.replace(/[^\d]/g, ""))
+      .map(Number)
+      .reduce((a, e) => a * e);
+    total += nums;
+  }
+  return total;
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  let ins = input.match(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)/g);
+  let total = 0;
+  let calc = true;
+  for (let mul of ins) {
+    if (mul === "do()") calc = true;
+    else if (mul === "don't()") calc = false;
+    else if (calc) {
+      let nums = mul
+        .split(",")
+        .map((e) => e.replace(/[^\d]/g, ""))
+        .map(Number)
+        .reduce((a, e) => a * e);
+      total += nums;
+    }
+  }
+  return total;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
+        expected: 161,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+        expected: 48,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  // onlyTests: true,
+});
